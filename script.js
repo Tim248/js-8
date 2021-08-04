ea9e94a63e119964d23f678ad1355ec4b8ad2581
Tim <tnematjonov@mail.ru>
2021-08-04 20:46:17 +0500

    end

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let users = {
-}
+let users = {};
 
-for (let i = 1; i < 11; i++) {
+for (let i = 1; i <= 1; i++) {
     let userName = prompt('Введите имя');
     let userAge = +prompt('Введите возраст');
-    users[i] = {name: userName, age: userAge}
-} 
+    users[i] = {
+        name: userName,
+        age: userAge
+    };
+}
+for (let key in users) {
+    console.log(`Пользователь ${key}`);
+    for (let newKey in users[key]) {
+        console.log(newKey == 'name' ? `Имя ${users[key][newKey]}` : `Возраст ${users[key][newKey]}`);
+    }
+}
 console.log(users);
